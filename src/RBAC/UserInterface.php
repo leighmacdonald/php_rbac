c32073e7a0826a5f0a0cbff9b88f9e6b23d5bce5
Leigh MacDonald <leigh@cudd.li>
2013-04-29 00:47:34 -0600

    Minor refactor

--- src/RBAC/UserInterface.php
+++ src/RBAC/UserInterface.php
@@ -10,7 +10,7 @@ use RBAC\Role\RoleSet;
 /**
  * To be able to associate your users with roles you must define the following minimal interface
  */
-interface SubjectInterface
+interface UserInterface
 {
     /**
      * This should return the unsigned unique ID of the user instance.
